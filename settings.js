337146985fc26f2c4f93c362c8f481c62e6935da
jayabro290-sketch <jayabro290@gmail.com>
2026-04-30 09:16:21 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  SESSION_ID: "", // Ice~A5XwDE "*"
+  OWNER_NUMBER: "263784263228", // Put Owner Number Here 
+  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
+  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+};
+
+// CREATED BY ice master tech ( MR abro OFC 🇿🇼 )
